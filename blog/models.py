5283839d272536f8f7f92e292f946aed3356029a
Umang <umangjparmar@gmail.com>
2018-07-28 23:23:21 +0530

    Add Comment model

--- blog/models.py
+++ blog/models.py
@@ -17,3 +17,17 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey('blog.Post', on_delete=models.CASCADE, related_name='comments')
+    author = models.CharField(max_length=15)
+    msg = models.CharField(max_length=300)
+    created_date = models.DateTimeField(default=timezone.now)
+    comment_visibility = models.BooleanField(default=True)
+
+    def unapprove(self):
+        self.comment_visibility = False
+        self.save()
+
+    def __str__(self):
+        return self.msg
