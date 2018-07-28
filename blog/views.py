2b6b51a6df386c6dcb9349d1a60ea455ddf75c39
Umang <umangjparmar@gmail.com>
2018-07-28 23:55:45 +0530

    Add forms to add post and comments

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from django.shortcuts import redirect
+from .forms import PostForm
 from django.contrib.auth.decorators import login_required
 from .models import Post,Comment
 
