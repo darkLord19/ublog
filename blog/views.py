2271344e067da7aea6acfc28b33880b0fea7417b
Umang <umangjparmar@gmail.com>
2018-07-28 23:47:53 +0530

    Added templates

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post,Comment
 
-# Create your views here.
+def home_page(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/posts.html', {'posts':posts})
