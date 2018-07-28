d2a83a2ecc2df2b493dc72e9308323af2fb4ee53
Umang <umangjparmar@gmail.com>
2018-07-29 01:56:50 +0530

    Add admin task for post delete and edit

--- blog/urls.py
+++ blog/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('post/new', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
     path('drafts/', views.post_draft_list, name='post_draft_list'),
-    path('post/<int:pk>/publish/', views.post_publish, name='post_publish'),
+    path('post/publish/<int:pk>', views.post_publish, name='post_publish'),
     path('post/<int:pk>/remove/', views.post_remove, name='post_remove'),
 ]
