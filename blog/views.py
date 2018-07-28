a6b8fc7fd25548e9e4f6b016a843ce51646dfe8f
Umang <umangjparmar@gmail.com>
2018-07-28 23:50:21 +0530

    Add view returning full blog post

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post,Comment
 
 def home_page(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/posts.html', {'posts':posts})
+
+def post_details(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_details.html', {'post': post})
