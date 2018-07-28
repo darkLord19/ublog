95669376421445fe76dece3766ab14c3f32e2986
Umang <umangjparmar@gmail.com>
2018-07-28 23:21:23 +0530

    Add Post model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Post(models.Model):
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    title = models.CharField(max_length=70)
+    body = models.TextField()
+    summary = models.CharField(max_length=100)
+    created_date = models.DateTimeField(default=timezone.now())
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        tmp = str(self.body)
+        self.summary = tmp[:100]
+        self.save()
+
+    def __str__(self):
+        return self.title
