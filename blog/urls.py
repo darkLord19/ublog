8b1143c80971974fca509ed83ca528ef7cb78d1c
Umang <umangjparmar@gmail.com>
2018-07-28 23:48:47 +0530

    Add url entries

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,12 @@ from . import views
 
 urlpatterns = [
     path('',views.home_page,name='home_page'),
+    path('contact', views.contact_me, name='contact_me'),
+    path('about', views.about_me, name='about_me'),
+    path('post/<int:pk>/', views.post_details, name='post_details'),
+    path('post/new', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('drafts/', views.post_draft_list, name='post_draft_list'),
+    path('post/<int:pk>/publish/', views.post_publish, name='post_publish'),
+    path('post/<int:pk>/remove/', views.post_remove, name='post_remove'),
 ]
