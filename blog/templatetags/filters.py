2c7c8b00913b3eb63c72a862e8ee9e1ad7680549
Umang Parmar <umangjparmar@gmail.com>
2018-10-02 00:23:52 +0530

    Add custom filter to get int coressponding to month

--- blog/templatetags/filters.py
+++ blog/templatetags/filters.py
@@ -5,3 +5,10 @@ register = template.Library()
 @register.filter(name='get_months_for_year')
 def get_months_for_year(dictionary, key):
     return dictionary.get(key)
+
+@register.filter(name='get_int_by_month')
+def get_int_by_month(m):
+    month = {'January':1, 'February':2, 'March':3, 'April':4, 'May':5, 'June':6,
+            'July':7, 'August':8, 'September':9, 'October':10, 'November':11, 'December':12
+    }
+    return month[m]
