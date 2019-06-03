87a0e37ea993fd604c7f6544085dd377e7d6abe4
Umang Parmar <umangjparmar@gmail.com>
2019-06-03 11:50:40 +0530

    Update Algolia index model

--- blog/index.py
+++ blog/index.py
@@ -5,5 +5,12 @@ from .models import Post
 
 @register(Post)
 class PostIndex(AlgoliaIndex):
-	fileds = ('title', 'body', 'tags')
-	
+	fileds = ('title', 'body', 'tags', 'categories')
+
+	settings = {
+		'searchableAttributes': ['title', 'body', 'tags'],
+		'queryType': 'prefixAll',
+		'highlightPreTag': '<mark>',
+		'highlightPostTag': '</mark>',
+		'hitsPerPage': 15
+	}
