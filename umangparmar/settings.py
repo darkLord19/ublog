5d9eedcf849d5bc135b1f860372da3c7aa4666a8
Umang Parmar <umangjparmar@gmail.com>
2020-11-10 23:00:56 +0530

    allow heroku host

--- umangparmar/settings.py
+++ umangparmar/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ublog.herokuapp.com']
 
 
 # Application definition
